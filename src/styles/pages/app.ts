4754d82885a1fd0ac9d556bc2a175ba111710241
Renato Souza <natoicm@gmail.com>
2023-03-28 23:08:21 -0300

    feat: add Header

---
+++ src/styles/pages/app.ts
@@ -0,0 +1,13 @@
+import { styled } from "@/styles/stitches.config";
+
+export const AppContainer = styled('div', {
+  width: '100%',
+  height: '100vh'
+})
+
+export const Header = styled('header', {
+  width: '100%',
+  maxWidth: '1168px',
+  padding: '4rem 0 3.2rem',
+  margin: '0 auto',
+})
